bddaaddbac66e5c0a54a7a147b577449d9b51f64
Nick Abel <nicholas.abel@bsc.es>
2025-07-28 13:47:02 +0200

    expanded override yaml

--- pipeline.py
+++ pipeline.py
@@ -6,15 +6,22 @@ from fabric import Connection
 with open("pipeline.yaml", "r") as f:
     cfg = yaml.safe_load(f)
 
-username = cfg["user"]["username"]
+ecmwf_username = cfg["user"]["ecmwf_username"]
+remote_username = cfg["user"]["remote_username"]
 remote_path = cfg["paths"]["remote_project_dir"]
 local_path = Path(cfg["paths"]["local_build_dir"])
-#TODO fill in intermediate steps
+
+ov = cfg["overrides"]
+
+ifs_source_git_url = ov["IFS_BUNDLE_IFS_SOURCE_GIT"].format(**ov)
+
 # Generate overrides.yaml
-#TODO expand for non-default cases
 (local_path / "overrides.yaml").write_text(f"""---
 environment:
-  - export DNB_SANDBOX_SUBDIR="{cfg['overrides']['DNB_SANDBOX_SUBDIR']}"
+  - export DNB_SANDBOX_SUBDIR="{ov['DNB_SANDBOX_SUBDIR']}"
+  - export DNB_IFSNEMO_URL="{ov['DNB_IFSNEMO_URL']}"
+  - export IFS_BUNDLE_IFS_SOURCE_VERSION="{ov['IFS_BUNDLE_IFS_SOURCE_VERSION']}"
+  - export IFS_BUNDLE_IFS_SOURCE_GIT="{ifs_source_git_url}"
 """)
 
 # Generate accounts.yaml
@@ -26,8 +33,8 @@ psubmit:
 """)
 #TODO fill in intermediate steps
 # Create tarball
-subprocess.run(["tar", "czvf", "../ifsnemo-build.tar.gz", "."], cwd=local_path)
-
-# Upload the tarball
-conn = Connection(f"{username}@glogin4.bsc.es")
-conn.put("../ifsnemo-build.tar.gz", remote=remote_path + "/ifsnemo-build.tar.gz")
+#subprocess.run(["tar", "czvf", "../ifsnemo-build.tar.gz", "."], cwd=local_path)
+#
+## Upload the tarball
+#conn = Connection(f"{remote_username}@glogin4.bsc.es")
+#conn.put("../ifsnemo-build.tar.gz", remote=remote_path + "/ifsnemo-build.tar.gz")
