d535f70227141774633753978c182c3589707df4
Nick Abel <nicholas.abel@bsc.es>
2025-07-28 14:26:09 +0200

    automate download and tarballing steps

--- pipeline.py
+++ pipeline.py
@@ -2,12 +2,40 @@ import yaml
 import subprocess
 from pathlib import Path
 from fabric import Connection
+import shutil
 
+
+############################################
+# 1.1 Ensure yq installed on local machine
+############################################
+if shutil.which("yq") is None:
+    print("""
+###########################################################################
+#      WARNING: 'yq' not found in PATH! Some steps may not work.          #
+# https://github.com/mikefarah/yq/releases/latest/download/yq_linux_amd64 #
+###########################################################################
+""")
+
+
+############################################
+# 1.2 Ensure netrc file exists
+############################################
+if not Path.home().joinpath('.netrc').exists():
+    print("""
+###########################################################
+# WARNING: '~/.netrc' not found! Some steps may not work. #
+###########################################################
+""")
+
+
+############################################
+# 1.3 Write ifsnemo-build config files
+############################################
 with open("pipeline.yaml", "r") as f:
     cfg = yaml.safe_load(f)
 
-ecmwf_username = cfg["user"]["ecmwf_username"]
 remote_username = cfg["user"]["remote_username"]
+remote_machine = cfg["user"]["remote_machine"]
 remote_path = cfg["paths"]["remote_project_dir"]
 local_path = Path(cfg["paths"]["local_build_dir"])
 
@@ -31,10 +59,23 @@ psubmit:
   account:     {cfg['psubmit']['account']}
   node_type:   {cfg['psubmit']['node_type']}
 """)
-#TODO fill in intermediate steps
+
+
+############################################
+# 1.4 Fetch and Package Build Artifacts
+############################################
+
+# Run './dnb.sh :du' from within local_path
+result = subprocess.run(['./dnb.sh', ':du'], cwd=local_path, capture_output=True, text=True)
+
+# Print the output
+print(result.stdout)
+if result.stderr:
+    print("Error output:", result.stderr)
+
 # Create tarball
-#subprocess.run(["tar", "czvf", "../ifsnemo-build.tar.gz", "."], cwd=local_path)
-#
+subprocess.run(["tar", "czvf", "../ifsnemo-build.tar.gz", "."], cwd=local_path)
+
 ## Upload the tarball
-#conn = Connection(f"{remote_username}@glogin4.bsc.es")
-#conn.put("../ifsnemo-build.tar.gz", remote=remote_path + "/ifsnemo-build.tar.gz")
+conn = Connection(f"{remote_username}@{remote_machine}")
+conn.put(f"{local_path}/../ifsnemo-build.tar.gz", remote=remote_path + "/ifsnemo-build.tar.gz")
