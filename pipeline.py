89f24456fa83b9251668cf02d172dceb2c563fac
Nick Abel <nicholas.abel@bsc.es>
2025-10-15 11:58:24 +0200

    DNB_IFSNEMO_USE_ARCH_AND_RAPS added

--- pipeline.py
+++ pipeline.py
@@ -178,6 +178,8 @@ def main(pipeline_yaml_path: str, skip_build: bool, no_run: bool):
             overrides_content.append(f'  - export DNB_IFSNEMO_WITH_GPU_EXTRA={ov.get("DNB_IFSNEMO_WITH_GPU_EXTRA")}')
         if ov.get('DNB_IFSNEMO_WITH_STATIC_LINKING'):
             overrides_content.append(f'  - export DNB_IFSNEMO_WITH_STATIC_LINKING={ov.get("DNB_IFSNEMO_WITH_STATIC_LINKING")}')
+        if ov.get('DNB_IFSNEMO_USE_ARCH_AND_RAPS'):
+            overrides_content.append(f'  - export DNB_IFSNEMO_USE_ARCH_AND_RAPS={ov.get("DNB_IFSNEMO_USE_ARCH_AND_RAPS")}')
 
 
         (local_path / "overrides.yaml").write_text('\n'.join(overrides_content) + '\n')
